63005d32d7331677a00b962bef9eb4d64bba2bff
Patrick Fahner <Fahner@stud.uni-heidelberg.de>
2015-08-05 01:39:34 +0200

    Chapter structure written

--- scripts/updateInput.sh
+++ scripts/updateInput.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
+rm -r latex/includes/*
+rm -r figures/analysis/*
 cp -r ../D0pmunuAnalysis/latest/latex/includes/ latex/
+cp -r ../D0pmunuAnalysis/latest/latex/figs/* figures/analysis/
 
